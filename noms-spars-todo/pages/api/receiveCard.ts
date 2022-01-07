3e39571c7be86af910238ab375cd540bf4a8301e
Samuel Thompson <thomps9012@gmail.com>
2022-01-07 15:54:18 -0500

    fixed parsing err

--- noms-spars-todo/pages/api/receiveCard.ts
+++ noms-spars-todo/pages/api/receiveCard.ts
@@ -3,17 +3,16 @@ import { connectToDatabase } from "../../util/mongodb";
 
 export default async function handler(req: {body: any}, res: { json: (arg0: any) => void; }) {
     const { client } = await connectToDatabase();
-    console.log(req.body)
+    let data = JSON.parse(req.body)
     const response = await client.db('spars_cmhs').collection('clients').updateOne(
-        { _id: new ObjectId(req.body.recordId) },
+        { _id: new ObjectId(data.recordId) },
         { $set: {
             gift_card_received: true,
-            NORA_acknowledged: req.body.nora,
-            client_acknowledged: req.body.client,
-            card_type: req.body.card_type,
-            card_amt: req.body.card_amt
+            NORA_acknowledged: data.nora,
+            client_acknowledged: data.client,
+            card_type: data.card_type,
+            card_amt: data.card_amt
         }}
     )
-    console.log(response)
     res.json(response)
 }
