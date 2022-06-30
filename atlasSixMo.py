51773d34f186b1b34d35ae5fb2c3fe19f43058ce
Samuel Thompson <thomps9012@gmail.com>
2022-06-30 13:50:57 -0400

    updated html imports

--- atlasSixMo.py
+++ atlasSixMo.py
@@ -1,76 +1,10 @@
-# import datetime
 import sys
 sys.path.append("")
-# from pprint import pprint
-# from pymongo import MongoClient
-# from datetime import timedelta
-
 from createHtml import createHtml, createHtmlList
 from getInterviewData import getInterviews, FilterInterviews
-        # db = dbclient.get_database('noms-interviews')
-        # intake = db.get_collection('intake')
-        # six_month = db.get_collection('6month')
-
-# ready to be imported
-# open_window = datetime.datetime.utcnow() + timedelta(weeks=-22)
-# print('open window: ', open_window.isoformat())
-# close_alert = open_window + timedelta(weeks=-4)
-# print('close window: ', close_alert.isoformat())
-
-
-
-# class GetInterviews:
-#     def getOpenWindows():
-#         six_month_open = intake.find({
-#             'interview_info.interviewDate': {'$gte': close_alert.isoformat()},
-#             'interview_info.interviewDate': {'$lt': open_window.isoformat()},
-#         }, {'client_information': 1, 'spars_entry': 1})
-#         print('six month open:')
-#         for doc in six_month_open:
-#             pprint(doc.get('_id').__str__())
-#         six_month_close = intake.find({
-#             'interview_info.interviewDate': {'$lt': close_alert.isoformat()}
-#         }, {'client_information': 1, 'spars_entry': 1})
-#         print('six month close:')
-#         for item in six_month_close:
-#             pprint(item.get('_id').__str__())
-#         return six_month_open, six_month_close
-#     def getSixMonthComplete():
-#         six_month_complete = six_month.find({'spars_entry': True}, {'_id': 1})
-#         print('six month complete:')
-#         for item in six_month_complete:
-#             pprint(item)
-#         return six_month_complete
-
-# class FilterInterviews:
-#     def filterSixMonth():
-#         six_month_open, six_month_close = GetInterviews.getOpenWindows()
-#         six_month_complete = GetInterviews.getSixMonthComplete()
-#         six_month_open_filtered = []
-#         six_month_close_filtered = []
-#         six_month_complete_arr = []
-#         six_month_open = six_month_open.clone()
-#         six_month_close = six_month_close.clone()
-#         six_month_complete = six_month_complete.clone()
-#         for item in six_month_complete:
-#             six_month_complete_arr.append(item.get('_id').__str__())
-#         for item in six_month_open:
-#             if item.get('_id').__str__() not in six_month_complete_arr:
-#                 six_month_open_filtered.append(item)
-#         print('six month open filtered:')
-#         for item in six_month_open_filtered:
-#             pprint(item.get('_id').__str__())
-#         for item in six_month_close:
-#             if item.get('_id').__str__() not in six_month_complete_arr:
-#                 six_month_close_filtered.append(item)
-#         print('six month close filtered:')
-#         for item in six_month_close_filtered:
-#             pprint(item.get('_id').__str__())
-#         return six_month_close_filtered, six_month_open_filtered
-# six_month_close_filtered, six_month_open_filtered = FilterInterviews.filterSixMonth()
 
 open_data, close_data, complete_data = getInterviews.getData('intake', '6month')
-six_month_close_filtered, six_month_open_filtered = FilterInterviews.filter(open_data, close_data, complete_data)
+six_month_open_filtered, six_month_close_filtered = FilterInterviews.filter(open_data, close_data, complete_data)
 
 open_list = createHtmlList.createOpenList(six_month_open_filtered)
 close_list = createHtmlList.createCloseList(six_month_close_filtered)
--- getInterviewData.py
+++ getInterviewData.py
@@ -10,6 +10,8 @@ print('close window: ', close_alert.isoformat())
 
 class getInterviews:
     def getData(collection1, collection2):
+        print('collection1: ', collection1)
+        print('collection2: ', collection2)
         dbclient = MongoClient('mongodb+srv://spars01:H0YXCAGHoUihHcSZ@cluster0.wuezj.mongodb.net/noms-interviews?retryWrites=true&w=majority')
         db = dbclient.get_database("noms-interviews")
         first_col = db.get_collection(collection1)
@@ -18,14 +20,23 @@ class getInterviews:
             'interview_info.interviewDate': {'$gte': close_alert.isoformat()},
             'interview_info.interviewDate': {'$lt': open_window.isoformat()},
         }, {'client_information': 1, 'spars_entry': 1})
+        print('open data:')
+        for item in open_data:
+            pprint(item.get('_id').__str__())
         close_data = first_col.find({
             'interview_info.interviewDate': {'$lt': close_alert.isoformat()}
         }, {'client_information': 1, 'spars_entry': 1})
+        print('close data:')
+        for item in close_data:
+            pprint(item.get('_id').__str__())
         complete_data = second_col.find({'spars_entry': True}, {'_id': 1})
         return open_data, close_data, complete_data
 
 class FilterInterviews:
     def filter(open_data, close_data, complete_data):
+        open_data = open_data.clone()
+        close_data = close_data.clone()
+        complete_data = complete_data.clone()
         open_filtered = []
         close_filtered = []
         complete_filtered = []
@@ -34,7 +45,13 @@ class FilterInterviews:
         for item in open_data:
             if item.get('_id').__str__() not in complete_filtered:
                 open_filtered.append(item)
+        print('open filtered:')
+        for item in open_filtered:
+            pprint(item.get('_id').__str__())
         for item in close_data:
             if item.get('_id').__str__() not in complete_filtered:
                 close_filtered.append(item)
+        print('close filtered:')
+        for item in close_filtered:
+            pprint(item.get('_id').__str__())
         return open_filtered, close_filtered
